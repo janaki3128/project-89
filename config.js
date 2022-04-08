23f84b0081dda8451fe49ca83ca5c9f38d8f303b
janaki3128 <79641763+janaki3128@users.noreply.github.com>
2022-04-08 14:39:40 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-RuJa8nUK3LUQOq7nHfh18lkJp9s4QxA",
+  authDomain: "spectagram-stage-7-1498d.firebaseapp.com",
+  projectId: "spectagram-stage-7-1498d",
+  storageBucket: "spectagram-stage-7-1498d.appspot.com",
+  messagingSenderId: "504097170039",
+  appId: "1:504097170039:web:f18f748ec884041e855c72"
+};
